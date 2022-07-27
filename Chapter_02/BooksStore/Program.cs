f725fd61e2dea0fec9266a8a2a8b39633e7f50f1
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-07-27 08:41:56 +0400

    Submit the chapter 02 changes

---
+++ Chapter_02/BooksStore/Program.cs
@@ -0,0 +1,14 @@
+using BooksStore;
+using BooksStore.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
+
+await builder.Build().RunAsync();

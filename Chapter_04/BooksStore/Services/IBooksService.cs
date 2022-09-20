f06545b53d486a760719cbfe9ab7c298fab77718
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-09-20 18:58:05 +0400

    Add the chapter 04 content

---
+++ Chapter_04/BooksStore/Services/IBooksService.cs
@@ -0,0 +1,9 @@
+﻿using BooksStore.Models;
+
+namespace BooksStore.Services
+{
+    public interface IBooksService
+    {
+        Task<List<Book>> GetAllBooksAsync(); 
+    }
+}
---
+++ Chapter_04/BooksStore/Services/LocalBooksService.cs
@@ -0,0 +1,32 @@
+﻿using BooksStore.Models;
+
+namespace BooksStore.Services
+{
+    public class LocalBooksService : IBooksService
+    {
+        public Task<List<Book>> GetAllBooksAsync()
+        {
+            return Task.FromResult(new List<Book>
+            {
+                 new Book
+                 {
+                     AuthorName = "John Smith",
+                     PublishingDate = new DateTime(2021, 01, 12),
+                     Title = "Blazor WebAssembly Guide"
+                 },
+                 new Book
+                 {
+                     AuthorName = "John Smith",
+                     PublishingDate = new DateTime(2022, 03, 13),
+                     Title = "Mastering Blazor WebAssembly"
+                 },
+                 new Book
+                 {
+                     AuthorName = "John Smith",
+                     PublishingDate = new DateTime(2022, 08, 01),
+                     Title = "Learning Blazor from A to Z"
+                 }
+        });
+        }
+    }
+}

7aef4b18def4ed5c371c22b906def74105cad453
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-11-10 13:52:00 +0400

    Submit chapter 05 content

---
+++ Chapter_05/ChapterContent/BooksStore/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BooksStore.Models
+{
+	public class Book
+	{
+		public string? Title { get; set; }
+        public string? AuthorName { get; set; }
+        public DateTime PublishingDate { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? Id { get; set; }
+        public int PagesCount { get; set; }
+    }
+}

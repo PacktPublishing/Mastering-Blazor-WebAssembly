c052e97012962d5a7d878c2d994321e05fdc378c
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-12-27 14:16:46 +0400

    Add the chapter 07 code

---
+++ Chapter_06/ChapterContent/BooksStore/wwwroot/scripts.js
@@ -0,0 +1,8 @@
+export function getRandomBook() {
+    // Return a JSON object with similar properties to our C# book model existing in the models folder
+    return {
+        title: 'Mastering Blazor WebAssembly',
+        author: 'Ahmad Mozaffar',
+        price: 49.99
+    }
+}

ae060ab46ac896b80bb5e18077856e1aa02d64d6
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-12-28 04:15:43 +0400

    Persist the app state using the browser's local storage

--- Chapter_07/ChapterContent/BooksStore/Program.cs
+++ Chapter_07/ChapterContent/BooksStore/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IBooksService, LocalBooksService>();
+
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();

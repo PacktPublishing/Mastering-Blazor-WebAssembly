5ce9e697a297479fdc8ee7a0c2f5c875306bf0eb
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-01-09 04:32:08 +0400

    Add the IBooksService and migrate the project to .NET 7.0 in Chapter 02

--- Chapter_02/BooksStore/Program.cs
+++ Chapter_02/BooksStore/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
+builder.Services.AddScoped<IBooksService, LocalBooksService>(); 
 
 await builder.Build().RunAsync();
---
+++ Chapter_02/BooksStore/Services/IBooksService.cs
@@ -0,0 +1,8 @@
+﻿using BooksStore.Models;
+
+namespace BooksStore.Services;
+
+public interface IBooksService
+{
+	Task<List<Book>> GetAllBooksAsync();
+}

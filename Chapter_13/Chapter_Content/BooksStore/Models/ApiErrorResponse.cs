059e2b595846eba3c1a52c466a793b910d41212d
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-04-30 12:46:04 +0400

    Submit the code of chapter 13

---
+++ Chapter_13/Chapter_Content/BooksStore/Models/ApiErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BooksStore.Models;
+
+public class ApiErrorResponse
+{
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    [JsonPropertyName("errors")]
+    public string[]? Errors { get; set; }
+}

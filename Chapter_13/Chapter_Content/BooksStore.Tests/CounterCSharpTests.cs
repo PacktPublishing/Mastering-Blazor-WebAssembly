9b930027fd397280f617429f19b7a157f7a2ad7d
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-04-30 19:47:11 +0400

    Update the content of chapter 13

--- Chapter_13/Chapter_Content/BooksStore.Tests/CounterCSharpTests.cs
+++ Chapter_13/Chapter_Content/BooksStore.Tests/CounterCSharpTests.cs
@@ -11,7 +11,7 @@ public class CounterCSharpTests : TestContext
 	{
 		// Arrange
 		var cut = RenderComponent<Counter>();
-
+		
 		// Assert that content of the paragraph shows counter at zero
 		cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
 	}

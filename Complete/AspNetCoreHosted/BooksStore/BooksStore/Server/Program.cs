b24785938ef4fbd5bfaf7cdf8ed2843cc14e7413
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-05-18 14:20:37 +0400

    Resolve the issues in the Blazor ASP.NET Core Hosted version

--- Complete/AspNetCoreHosted/BooksStore/BooksStore/Server/Program.cs
+++ Complete/AspNetCoreHosted/BooksStore/BooksStore/Server/Program.cs
@@ -10,7 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -39,14 +41,28 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseWebAssemblyDebugging();
 }
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseHttpsRedirection();
+else
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
 app.UseAuthorization();
 
+
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
